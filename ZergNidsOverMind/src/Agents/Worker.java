363601c4162b7d6d0624d2f5c8e06165fd6ec458
Simon Varvaressos <varvaressossimon@gmail.com>
2013-04-16 01:13:43 -0400

    Création d'un agent + Config pour utilisation de Jade
    Changer les Run config pour que Jade fonctionne.
    L'agent worker écrit "Yo de worker" au setup

---
+++ ZergNidsOverMind/src/Agents/Worker.java
@@ -0,0 +1,15 @@
+package Agents;
+import jade.core.Agent;
+import jade.core.AID;
+
+public class Worker extends Agent{
+	
+	
+	protected void setup()
+	{
+		System.out.print("Yo de Worker");
+	}
+	
+	
+	
+}
